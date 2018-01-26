a294ad091a0d12d28d5a51f7e2e00c15fb5f496c
Holger Stitz <holger.stitz@jku.at>
2018-01-26 16:37:19 +0100

    Restore panel state

--- src/index.ts
+++ src/index.ts
@@ -72,23 +72,37 @@ class XkcdWidget extends Widget {
 /**
  * Activate the xckd widget extension.
  */
-function activate(app: JupyterLab, palette: ICommandPalette) {
+function activate(
+  app: JupyterLab,
+  palette: ICommandPalette,
+  restorer: ILayoutRestorer
+) {
   console.log("JupyterLab extension jupyterlab_xkcd is activated!");
 
-  // Create a single widget
-  let widget: XkcdWidget = new XkcdWidget();
+  // Declare a widget variable
+  let widget: XkcdWidget;
 
   // Add an application command
   const command: string = "xkcd:open";
   app.commands.addCommand(command, {
     label: "Random xkcd comic",
     execute: () => {
+      if (!widget) {
+        // Create a new widget if one does not exist
+        widget = new XkcdWidget();
+        widget.update();
+      }
+      if (!tracker.has(widget)) {
+        // Track the state of the widget for later restoration
+        tracker.add(widget);
+      }
       if (!widget.isAttached) {
         // Attach the widget to the main area if it's not there
         app.shell.addToMainArea(widget);
+      } else {
+        // Refresh the comic in the widget
+        widget.update();
       }
-      // Refresh the comic in the widget
-      widget.update();
       // Activate the widget
       app.shell.activateById(widget.id);
     }
@@ -96,6 +110,14 @@ function activate(app: JupyterLab, palette: ICommandPalette) {
 
   // Add the command to the palette.
   palette.addItem({ command, category: "Tutorial" });
+
+  // Track and restore the widget state
+  let tracker = new InstanceTracker<Widget>({ namespace: "xkcd" });
+  restorer.restore(tracker, {
+    command,
+    args: () => JSONExt.emptyObject,
+    name: () => "xkcd"
+  });
 }
 
 /**
